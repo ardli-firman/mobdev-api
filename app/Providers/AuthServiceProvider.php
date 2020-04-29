878d7305650ff8b4a5b2bae36498801bb7152241
ardlifirman <estungtung59@gmail.com>
2020-04-30 00:00:31 +0700

    token expired test

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
@@ -26,6 +27,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Passport::routes();
+        Passport::tokensExpireIn(Carbon::now()->addMinute(2));
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(1));
         //
     }
 }
