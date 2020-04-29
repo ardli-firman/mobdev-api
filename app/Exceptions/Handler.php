ac932bb7cf1ebf4c73e4166a04f4b393aeffe9e5
ardlifirman <estungtung59@gmail.com>
2020-04-30 00:36:13 +0700

    fix response not valid credentials uwu

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Laravel\Passport\Exceptions\OAuthServerException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -50,6 +51,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof OAuthServerException) {
+            return response()->json(['message' => 'Authentication credentials not valid'], 403);
+        }
         return parent::render($request, $exception);
     }
 }
